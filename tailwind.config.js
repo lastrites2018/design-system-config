fff053b82db0a29d7a9d37a751c190d2fe051631
SoYoung210 <ethdud1@gmail.com>
2021-01-01 23:27:40 +0900

    ci: add tailiwnd config

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  theme: {
+    colors: {
+      gray: colors.coolGray,
+      blue: colors.lightBlue,
+      red: colors.rose,
+      pink: colors.fuchsia,
+    },
+  },
+}
+;
