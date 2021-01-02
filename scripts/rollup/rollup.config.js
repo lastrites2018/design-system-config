cd3478d9012d37fa45a16eedfb81d8e7e828a73b
SoYoung210 <ethdud1@gmail.com>
2021-01-02 19:22:58 +0900

    ci: add commonjs plugin

--- scripts/rollup/rollup.config.js
+++ scripts/rollup/rollup.config.js
@@ -1,4 +1,5 @@
 import babel from '@rollup/plugin-babel';
+import commonjs from '@rollup/plugin-commonjs';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import postcss from 'rollup-plugin-postcss';
@@ -17,15 +18,18 @@ export default inputSrc
         format: 'es',
       },
       plugins: [
-        peerDepsExternal(),
-        nodeResolve({
-          extensions,
-        }),
         babel({
           babelHelpers: 'runtime',
           exclude: 'node_modules/**',
           extensions,
         }),
+        nodeResolve({
+          extensions,
+        }),
+        commonjs({
+          extensions: [...extensions, '.js'],
+        }),
+        peerDepsExternal(),
         postcss({
           plugins: [],
         }),
