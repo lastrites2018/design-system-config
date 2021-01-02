6fc4a5a16dba91fc9c9d17968bcb3c617707a688
SoYoung210 <ethdud1@gmail.com>
2021-01-02 19:24:05 +0900

    chore: add comment

--- scripts/rollup/rollup.config.js
+++ scripts/rollup/rollup.config.js
@@ -26,6 +26,9 @@ export default inputSrc
         nodeResolve({
           extensions,
         }),
+        // https://velog.io/@peterkimzz/rollup-%ED%94%8C%EB%9F%AC%EA%B7%B8%EC%9D%B8
+        // commonJS 로 작성된 모듈들을 번들링 결과물에 포함시키려고 할 때 문제가 발생
+        // CommonJS 로 작성된 모듈들을 ES6 바꾸어서 rollup이 해석할 수 있게 도와줍니다.
         commonjs({
           extensions: [...extensions, '.js'],
         }),
